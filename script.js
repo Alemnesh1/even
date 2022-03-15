6e13b4b5996d6f79068f44f112c83ba3f107d578
AlemneshD <101061602+Alemnesh1@users.noreply.github.com>
2022-03-15 05:14:21 +0000

    I write comment

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let number = parseInt(prompt('choose a number'));
+//parseInt convert input in to number
+
  if (number % 2===0)
+//identify if the number is even
   console.log(`${number} is even `);
+//print even to console
 else
-// console.log(`${number} is odd `);
-alert('odd')
+ console.log(`${number} is odd `);
+//print odd in to consolel
